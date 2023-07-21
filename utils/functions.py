5108d2a2bdd796b87ba2908e64a1925932b32f3d
Jinjun Liu <ljj7129@gmail.com>
2023-07-20 23:34:34 -0500

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -65,8 +65,8 @@ def test(config, logger, epoch, model, test_loader, criterion):
                 _, axarr = plt.subplots(2, targets.shape[1],
                                         figsize=(targets.shape[1] * 5, 10))
                 for t in range(targets.shape[1]):
-                    axarr[0][t].imshow(targets[0, t, 0].detach().cpu().numpy(), cmap='gray')
-                    axarr[1][t].imshow(outputs[0, t, 0].detach().cpu().numpy(), cmap='gray')
+                    axarr[0][t].imshow(targets[0, t, 0].detach().cpu().numpy(), cmap='jet')
+                    axarr[1][t].imshow(outputs[0, t, 0].detach().cpu().numpy(), cmap='jet')
                 plt.savefig(os.path.join(config.cache_dir, '{:03d}_{:05d}.png'.format(epoch, batch_idx)))
                 plt.close()
     return epoch_records
