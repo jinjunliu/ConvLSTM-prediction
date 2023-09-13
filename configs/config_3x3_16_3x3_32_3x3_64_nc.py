f6a16d068065bdd474b28e4dbec19ddc6749eaef
Jinjun Liu <ljj7129@gmail.com>
2023-09-13 01:33:22 -0500

    Update config_3x3_16_3x3_32_3x3_64_nc.py

--- configs/config_3x3_16_3x3_32_3x3_64_nc.py
+++ configs/config_3x3_16_3x3_32_3x3_64_nc.py
@@ -18,9 +18,9 @@ class Config:
         # train_batch_size = 64
         # valid_batch_size = 2 * train_batch_size
         # test_batch_size = 2 * train_batch_size
-        train_batch_size = 32
-        valid_batch_size = train_batch_size
-        test_batch_size = train_batch_size
+        train_batch_size = 16
+        valid_batch_size = 2*train_batch_size
+        test_batch_size = 2*train_batch_size
     else:
         num_workers = 0
         train_batch_size = 2
@@ -42,19 +42,33 @@ class Config:
     epochs = 20
 
     # (type, activation, in_ch, out_ch, kernel_size, padding, stride)
-    encoder = [('conv', 'leaky', 1, 16, 3, 1, 2),
-             ('convlstm', '', 16, 16, 3, 1, 1),
-             ('conv', 'leaky', 16, 32, 3, 1, 2),
+    # encoder = [('conv', 'leaky', 1, 16, 3, 1, 2),
+    #          ('convlstm', '', 16, 16, 3, 1, 1),
+    #          ('conv', 'leaky', 16, 32, 3, 1, 2),
+    #          ('convlstm', '', 32, 32, 3, 1, 1),
+    #          ('conv', 'leaky', 32, 64, 3, 1, 2),
+    #          ('convlstm', '', 64, 64, 3, 1, 1)]
+    # decoder = [('deconv', 'leaky', 64, 32, 4, 1, 2),
+    #            ('convlstm', '', 64, 32, 3, 1, 1),
+    #            ('deconv', 'leaky', 32, 16, 4, 1, 2),
+    #            ('convlstm', '', 32, 16, 3, 1, 1),
+    #            ('deconv', 'leaky', 16, 16, 4, 1, 2),
+    #            ('convlstm', '', 17, 16, 3, 1, 1),
+    #            ('conv', 'sigmoid', 16, 1, 1, 0, 1)]
+
+    encoder = [('conv', 'leaky', 1, 32, 3, 1, 2),
              ('convlstm', '', 32, 32, 3, 1, 1),
              ('conv', 'leaky', 32, 64, 3, 1, 2),
-             ('convlstm', '', 64, 64, 3, 1, 1)]
-    decoder = [('deconv', 'leaky', 64, 32, 4, 1, 2),
+             ('convlstm', '', 64, 64, 3, 1, 1),
+             ('conv', 'leaky', 64, 128, 3, 1, 2),
+             ('convlstm', '', 128, 128, 3, 1, 1)]
+    decoder = [('deconv', 'leaky', 128, 64, 4, 1, 2),
+               ('convlstm', '', 128, 64, 3, 1, 1),
+               ('deconv', 'leaky', 64, 32, 4, 1, 2),
                ('convlstm', '', 64, 32, 3, 1, 1),
-               ('deconv', 'leaky', 32, 16, 4, 1, 2),
-               ('convlstm', '', 32, 16, 3, 1, 1),
-               ('deconv', 'leaky', 16, 16, 4, 1, 2),
-               ('convlstm', '', 17, 16, 3, 1, 1),
-               ('conv', 'sigmoid', 16, 1, 1, 0, 1)]
+               ('deconv', 'leaky', 32, 32, 4, 1, 2),
+               ('convlstm', '', 33, 32, 3, 1, 1),
+               ('conv', 'sigmoid', 32, 1, 1, 0, 1)]
 
     data_dir = os.path.join(root_dir, 'data')
     output_dir = os.path.join(root_dir, 'output')
